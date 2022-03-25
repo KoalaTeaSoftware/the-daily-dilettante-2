1559377554934208b3d411a6c3fb1b429c96b547
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-03-25 06:45:35 +0000

    specify baseURL, and remove domain from the test file

--- cypress/integration/smoke.js
+++ cypress/integration/smoke.js
@@ -1,6 +1,6 @@
 describe("Smoke Tests", ()=>{
     it("Has the correct title tag", ()=>{
-        cy.visit("http://localhost:8080")
+        cy.visit("/")
         cy.title().should("contain", "the-daily-dilettante-2")
     })
 })
