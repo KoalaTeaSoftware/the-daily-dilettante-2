cbeba231300e00a41ce6ab9c84f1f169c3a4e2b6
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-03-30 06:26:43 +0100

    tiddly tweaks to see if they get to stage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your sample Vue.js App"/>
+    <HelloWorld msg="Welcome to Your new Vue.js App"/>
   </div>
 </template>
 
