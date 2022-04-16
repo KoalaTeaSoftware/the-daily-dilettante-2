a02501948a6f949edf9a3ae9cd9ac7c48d6bf699
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-04-16 08:24:42 +0100

    able to read from the Pages collection

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import { createApp } from 'vue'
+// this is needed to get Vue.js to generate things
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// this is what is needed to get firebase going, and to get access to the pages database
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, getDocs} from "firebase/firestore";
+import {getAnalytics} from "firebase/analytics";
+
+// ToDo: turn the API key into a 'secret'
+const firebaseConfig = {
+    apiKey: "AIzaSyBm4mMSmMw8g15ErVonc23nKDszjlEpipI",
+    authDomain: "daily-dilettante.firebaseapp.com",
+    projectId: "daily-dilettante",
+    storageBucket: "daily-dilettante.appspot.com",
+    messagingSenderId: "749678297903",
+    appId: "1:749678297903:web:2931caec98bc8fcbf057da",
+    measurementId: "G-RJK0G5SZ9Z"
+};
+
+// this makes a couple of objects that should be visible the world over
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// still don't know much about this
+const analytics = getAnalytics(app);
+
+// actually generate the site
 createApp(App)
     .use(router)
     .mount('#app')
 
+// this is a sample to prove that we can read fomr the pages
+getDocs(collection(db, "pages")).then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`)
+    });
+});
--- support/icons/logomaker.bat
+++ support/icons/logomaker.bat
@@ -5,3 +5,4 @@ magick favicon.fw.png -thumbnail 32x32   -alpha on -background none -flatten fav
 magick favicon.fw.png -thumbnail 16x16   -alpha on -background none -flatten fav/favicon-16x16.png
 magick favicon.fw.png -thumbnail 152x152 -alpha on -background none -flatten fav/apple-touch-icon-152x152.png
 magick favicon.fw.png -thumbnail 144x144 -alpha on -background none -flatten fav/msapplication-icon-144x144.png
+magick favicon.fw.png -thumbnail 192x192 -alpha on -background none -flatten fav/android-chrome-192x192.png
