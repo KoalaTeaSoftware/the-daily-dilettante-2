545021e4a5db6da5a5a7ebabb5d57cec64d3f875
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-05-19 07:49:44 +0100

    DD-16 Use Pinia for state management

--- src/components/SecondaryNav.vue
+++ src/components/SecondaryNav.vue
@@ -4,8 +4,8 @@
       <div class=""  id="secondary-nav">
         <div class="navbar-nav">
           <router-link to="/contact" class="nav-link active" aria-current="page">Contact</router-link>
-          <a v-show="showAsLoggedIn" type="button" id="logoutButton" class="nav-link" @click='logout'>Logout</a>
-          <a v-show="!showAsLoggedIn" type="button" id="startLoginButton" class="nav-link"
+          <a v-if="iAmLoggedIn" type="button" id="logoutButton" class="nav-link" @click='logout'>Logout</a>
+          <a v-else type="button" id="startLoginButton" class="nav-link"
              data-bs-toggle="modal" data-bs-target="#login-component">Login</a>
         </div>
       </div>
@@ -33,16 +33,6 @@ export default {
       UserIdentity.methods.logOut()
     }
   },
-  mounted() {
-    /*
-      In order to present the correct user-authorization button, this direct links in to firebaseAuth
-      It seems to be a struggle to achieve this dynamic control through properties, or shared data
-      Again, there may be a cleaner way
-     */
-    AUTH_HANDLE.onAuthStateChanged((user) => {
-      this.showAsLoggedIn = !!user;
-    });
-  }
 }
 </script>
 
