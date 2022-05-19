d158a4a6daf18f986bc7a87f1633d5e8e578b4c8
Mark Goldthorp <64464921+KoalaTeaSoftware@users.noreply.github.com>
2022-05-19 07:42:50 +0100

    DD-16 Use Pinia for state management

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 // this is needed to get Vue.js to generate things
+// noinspection SpellCheckingInspection
+
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// using the compat firebase stuff because we are using the latest versions (forget the details)
+// using the compat firebase stuff because we are using the latest versions of vue (forget the details)
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// ToDo: turn the API key into a 'secret'
+import {createPinia} from "pinia";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBm4mMSmMw8g15ErVonc23nKDszjlEpipI",
     authDomain: "daily-dilettante.firebaseapp.com",
@@ -22,14 +25,19 @@ const firebaseConfig = {
 // Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-// Make these available to all the components
+// ToDo: see if these really need to be shared around the world, or could be local to where they are needed
+// The DB handle will be used by the editable components, might as well get it once, and share that everywhere
 export const DB_HANDLE = firebaseApp.firestore();
 export const AUTH_HANDLE = firebase.auth();
 
 // still don't know much about this
 // const analytics = getAnalytics(APP_HANDLE);
 
-// actually generate the site
+// we need to be explicit about which pinia instance to use in the various components that need it
+export const pinia = createPinia()
+
+// actually generate the site, and the data store
 createApp(App)
-    .use(router)
+    .use(router, pinia)
     .mount('#app')
+
